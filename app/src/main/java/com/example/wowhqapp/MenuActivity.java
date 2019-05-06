2be42b9bec4a55e254e74cfe52e518e8f251d589
GeorgiyX <@GeorgiyX>
2019-05-06 21:35:33 +0300

    Start commit

---
+++ app/src/main/java/com/example/wowhqapp/MenuActivity.java
@@ -0,0 +1,40 @@
+package com.example.wowhqapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MenuActivity extends AppCompatActivity {
+
+    public static final String LAUNCH_AUCANDDEALS = "LAUNCH_AUCANDDEALS";
+    public static final String KEY_TYPE = "TYPE";
+    TextView textViewAuc;
+    TextView textViewDeals;
+    TextView textViewToken;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        textViewAuc= (TextView) findViewById(R.id.menu_auc);
+        textViewDeals = (TextView) findViewById(R.id.menu_deals);
+        textViewDeals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LAUNCH_AUCANDDEALS);
+                intent.putExtra(KEY_TYPE, false);
+                startActivity(intent);
+            }
+        });
+        textViewAuc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LAUNCH_AUCANDDEALS);
+                intent.putExtra(KEY_TYPE, true);
+                startActivity(intent);
+            }
+        });
+    }
+}
