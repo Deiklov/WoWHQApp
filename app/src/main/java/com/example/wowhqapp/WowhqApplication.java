62bd38e89cb16c7282f8fa268f075361f468baa5
Nicholas Eskov <mr.eskov1@yandex.ru>
2019-05-08 18:57:04 +0300

    First network TalentsApi created.

--- app/src/main/java/com/example/wowhqapp/WowhqApplication.java
+++ app/src/main/java/com/example/wowhqapp/WowhqApplication.java
@@ -5,15 +5,16 @@ import android.arch.persistence.room.Room;
 import android.content.Context;
 
 import com.example.wowhqapp.databases.database.TalentsCalculatorDatabase;
+import com.example.wowhqapp.network.ApiTalentsRepository;
 
 public class WowhqApplication extends Application {
     private TalentsCalculatorDatabase mTalentCalcDb;
-    // private ApiRepo mApiRepo;
+    private ApiTalentsRepository mApiTalentsRepo;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        // mApiRepo = new ApiRepo();
+        mApiTalentsRepo = new ApiTalentsRepository();
         mTalentCalcDb = Room.databaseBuilder(
                 this,
                 TalentsCalculatorDatabase.class,
@@ -26,9 +27,9 @@ public class WowhqApplication extends Application {
         return mTalentCalcDb;
     }
 
-    // public ApiRepo getApis() {
-    //     return mApiRepo;
-    // }
+    public ApiTalentsRepository getTalentsApi() {
+        return mApiTalentsRepo;
+    }
 
     public static WowhqApplication from(Context context) {
         return (WowhqApplication) context.getApplicationContext();
--- app/src/main/java/com/example/wowhqapp/network/WowSpecApi.java
+++ app/src/main/java/com/example/wowhqapp/network/WowSpecApi.java
@@ -1,4 +1,14 @@
 package com.example.wowhqapp.network;
 
+import com.example.wowhqapp.databases.entity.WowClass;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public interface WowSpecApi {
+    @GET("/api/specs/{lang}")
+    Call<List<WowClass>> getAll(@Path("lang") String lang);
 }
